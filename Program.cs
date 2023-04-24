6b20f7a702761d0c5d4f411342110b767f4a4769
vuductho96 <112962875+vuductho96@users.noreply.github.com>
2023-04-24 12:13:52 +0700

    error code

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace dotnet_IndexOutOfRangeException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            int index = 6;
+            try
+            {
+                int value = numbers[index];
+            
+            }
+            catch (System.ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("Index is out of range");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Please enter a valid index:");
+                index = int.Parse(Console.ReadLine());
+                int value = numbers[index];
+               
+            }
+        }
+    }
+}
